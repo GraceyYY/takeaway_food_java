6b1200cefdd3cb0fe0071e146ce228f6916c39fb
Grace <lunay0325@gmail.com>
2019-02-14 15:44:20 +0800

    Implement the Dish class to store dishes' information

---
+++ src/model/Dish.java
@@ -0,0 +1,39 @@
+package model;
+
+public class Dish {
+
+    private String name;
+    private String id;
+    private double price;
+
+    public Dish(String name, String id, double price) {
+        this.name = name;
+        this.id = id;
+        this.price = price;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getId() {
+        return this.id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public double getPrice() {
+        return this.price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+}
