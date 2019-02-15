78d7363919fba9f1aaab5b5e67887bd2b154e72c
Grace <lunay0325@gmail.com>
2019-02-15 23:11:45 +0800

    Edit the notice message of input order

--- src/tools/Input.java
+++ src/tools/Input.java
@@ -6,7 +6,7 @@ public class Input {
 
     public String[] takeOrder() {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("请输入所选菜品（例如：ITEM0001 * 2）,不同菜品请使用逗号隔开");
+        System.out.println("请输入所选菜品（例如：ITEM0001 x 2）,不同菜品请使用逗号隔开");
         String[] order = scanner.nextLine().split(",");
         return order;
     }
