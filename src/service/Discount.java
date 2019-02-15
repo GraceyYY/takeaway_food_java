1d54613b8067d2d272949c820e3a0d6c72e11e5a
Grace <lunay0325@gmail.com>
2019-02-16 02:23:59 +0800

    Implement the feature of selecting the best promotion according to the order

---
+++ src/service/Discount.java
@@ -0,0 +1,27 @@
+package service;
+
+import model.DeductOverFullPrice;
+import model.HalfPrice;
+
+public class Discount {
+    private String bestPromotion;
+    private double discount;
+
+    public Discount(Order order) {
+        DeductOverFullPrice deductOver30 = new DeductOverFullPrice("满30减6", "All");
+        HalfPrice halfPrice = new HalfPrice("指定菜品半价", "ITEM0001, ITEM0022");
+        double discountOver30 = deductOver30.discount(order);
+        double discountHalfPrice = halfPrice.discount(order);
+        if ( discountOver30 == 0 && discountHalfPrice == 0) {
+            this.bestPromotion = "none";
+            this.discount = 0;
+        } else if (discountOver30 >= discountHalfPrice) {
+            this.bestPromotion = deductOver30.getType();
+            this.discount = discountOver30;
+        } else {
+            this.bestPromotion = halfPrice.getType();
+            this.discount = discountHalfPrice;
+        }
+    }
+
+}
