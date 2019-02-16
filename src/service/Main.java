650ff77cbb64047a6f269174a0a684dbead88383
Grace <lunay0325@gmail.com>
2019-02-16 22:50:59 +0800

    Implement the take order and print method in the Main class

---
+++ src/service/Main.java
@@ -0,0 +1,16 @@
+package service;
+
+import tools.Input;
+import tools.Print;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Input input = new Input();
+        Order order = new Order(input.takeOrder());
+        Print output = new Print(order);
+        output.printOrder();
+        output.printPromotion();
+        output.printTotalPrice();
+    }
+}
